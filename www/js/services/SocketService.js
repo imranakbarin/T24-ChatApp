1e5a4e1718ba6dd7e6bc3554728cf6c158b8c14f
Imran Akbar <imranakbar@temenos.com>
2016-09-28 21:17:32 +0530

    Initial commit

---
+++ www/js/services/SocketService.js
@@ -0,0 +1,13 @@
+(function(){
+
+	angular.module('starter')
+	.service('SocketService', ['socketFactory', SocketService]);
+
+	function SocketService(socketFactory){
+		return socketFactory({
+			
+			ioSocket: io.connect('http://10.93.22.97:3000')
+
+		});
+	}
+})();
